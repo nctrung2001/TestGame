a84f0129289fd5d7e52db83fcb82b9d6da8a8aa4
nctrung2001 <nguyenchitrung2001@gmail.com>
2021-09-16 12:34:58 +0700

    write JSON

--- TestGame/Assets/_Project/Scenes/GamePlay/GamePlayController.cs
+++ TestGame/Assets/_Project/Scenes/GamePlay/GamePlayController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using SS.View;
+using System.IO;
 using TMPro;
 
 public class GamePlayController : Controller
@@ -18,23 +19,43 @@ public class GamePlayController : Controller
 
     private void Start()
     {
+        LevelData levelData = new LevelData();
+        levelData = LoadLevelData();
+
         btnBack.onClick.AddListener(() => Manager.Add(PopupConfirmController.POPUPCONFIRM_SCENE_NAME));
         btnWin.onClick.AddListener(() =>
         {
-            
         });
 
-        LevelData levelData = new LevelData();
-        levelData.id = 1;
-        levelData.name = "Level 1";
-        levelData.description = "Nothing here";
+        Debug.Log(levelData);
+    
+        // Debug.Log("Level: " + levelData.id);
+        // Debug.Log("name: " + levelData.name);
+        // Debug.Log("description: " + levelData.description);
 
-        string json = JsonUtility.ToJson(levelData, true);
-        LevelData levelLoaded = JsonUtility.FromJson<LevelData>(json);
-        txtLevel.text = "Level: " +levelLoaded.id;
+        //LevelData levelData = new LevelData();
+        //levelData.id = 1;
+        //levelData.name = "Level 1";
+        //levelData.description = "Nothing here";
+
+        //string json = JsonUtility.ToJson(levelData, true);
+        //LevelData levelLoaded = JsonUtility.FromJson<LevelData>(json);
+        
     }
 
-    private void Update()
+    public static LevelData LoadLevelData()
     {
+        string path = Application.persistentDataPath + "/levelData.json";
+        var json = File.ReadAllText(path);
+        if (File.Exists(path))
+        {
+            LevelData levelData = JsonUtility.FromJson<LevelData>(json);
+            return levelData;
+        }
+        else
+        {
+            Debug.Log("File not found");
+            return null;
+        }
     }
 }
--- TestGame/Assets/_Project/Scenes/GenLevel/GenLevelController.cs
+++ TestGame/Assets/_Project/Scenes/GenLevel/GenLevelController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using System;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine.UI;
 using SS.View;
 using TMPro;
@@ -13,6 +13,7 @@ public class GenLevelController : Controller
     [SerializeField] private Button btnGen;
     [SerializeField] private TMP_InputField inpId, inpName, inpDescription;
     LevelData levelData;
+    //Dictionary<string,LevelData> levelList;
 
     public override string SceneName()
     {
@@ -21,38 +22,40 @@ public class GenLevelController : Controller
 
     private void Start()
     {
+        //levelList = new Dictionary<string, LevelData>();
         btnGen.onClick.AddListener(() =>
         {
             levelData = new LevelData();
             levelData.id = int.Parse(inpId.text);
             levelData.name = inpName.text;
             levelData.description = inpDescription.text;
+            //levelList.Add("level", levelData);
+            string jsonText = JsonUtility.ToJson(levelData, true);
+            SaveLevelData(jsonText);
+            Debug.Log(jsonText);
         });
     }
 
-    public void SaveLevelData()
+    public void SaveLevelData(string jsonText)
     {
-#if UNITY_EDITOR
-        string jsonText = JsonUtility.ToJson(this, true);
-        //string path = GetFilePath();
-        //File.WriteAllText(path, jsonText);
-#else
-        Debug.LogError("Can't save json data on mobile");
-#endif
+        string path = @"D:/Unity/Project/levelData.json";
+        File.WriteAllText(path, jsonText);
+        FileStream stream = new FileStream(path, FileMode.Create);
+        stream.Close();
     }
 
-    public static string GetFilePath(int storyId, int orderInStory)
-    {
-        return Path.Combine(Application.dataPath, $"Resources/{GetResPath(storyId, orderInStory)}" + ".json");
-    }
+    // public static string GetFilePath(int storyId, int orderInStory)
+    // {
+    //     return Path.Combine(Application.dataPath, $"Resources/{GetResPath(storyId, orderInStory)}" + ".json");
+    // }
 
-    public static string GetResPath(int storyId, int orderInStory)
-    {
-        return ($"LevelData/{GetPicDataFileName(storyId, orderInStory)}");
-    }
+    // public static string GetResPath(int storyId, int orderInStory)
+    // {
+    //     return ($"LevelData/{GetPicDataFileName(storyId, orderInStory)}");
+    // }
 
-    public static string GetPicDataFileName(int storyId, int orderInStory)
-    {
-        return $"{storyId}_{orderInStory}_Data";
-    }
+    // public static string GetPicDataFileName(int storyId, int orderInStory)
+    // {
+    //     return $"{storyId}_{orderInStory}_Data";
+    // }
 }
